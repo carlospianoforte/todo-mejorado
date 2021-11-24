12ba1702712016ae520f8bec70082c8724c0cfbc
Carlos Ortiz <carlospianiforte@gmail.com>
2021-11-24 00:58:01 -0500

    todo-mejorado2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
 import React from "react";
-import { AppUI } from "./AppUI";
-import { TodoProvider } from "./TodoContext";
+import { useTodos } from "./useTodos";
+import {TodoCounter} from "./TodoCounter";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoList } from "./TodoList";
+import { TodoSearch } from "./TodoSearch";
+import { Modal } from "./Modal";
+import { TodoForm } from "./TodoForm";
+import todo from "./images/todo.png"
+import { TodosLoading } from "./TodosLoading";
+import { TodosError } from "./TodosError";
+import { EmptyTodos } from "./EmptyTodos";
+import "./styles/App.css"
+import TodoHeader from "./TodoHeader";
 
 import "./styles/App.css"
 
 function App() {
+  const {
+    error,
+    loading, 
+    searchedTodos, 
+    completeTodo, 
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    addTodo
+  } = useTodos();
 
-  return (
+  return(
 
-    <TodoProvider>
-          <AppUI/>
-
-
-    </TodoProvider>
-
+    <>
+  
+      <img className="App-image" src={todo} alt="logo" />
     
+    <TodoHeader>
+      <TodoCounter
+        totalTodos={totalTodos}
+        completedTodos={completedTodos}
+      />
+      
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      /> 
+    </TodoHeader>
+    
+      <TodoList>
+        {error&& <TodosError error ={error}/>}
+        {loading && <TodosLoading/>}
+        {(!loading && !searchedTodos.length ) && <EmptyTodos/>}
+        {(!loading && searchedTodos.length<=0 ) && "No se encontro el Todo"}
+        {searchedTodos.map(todo=>(
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+            completeTodo={()=>completeTodo(todo.text)}
+            onDelete={()=>deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList> 
+
+
+      {!!openModal&&(
+            <Modal>
+                <TodoForm
+                  addTodo={addTodo}
+                  setOpenModal={setOpenModal}
+                />
+            </Modal>
+      )}
+
+    <CreateTodoButton
+      setOpenModal={setOpenModal} 
+    />
+    
+    
+    </>
+
+
   );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,9 @@
 import React from "react";
 import "./styles/TodoSearch.css"
-import { TodoContext } from "./TodoContext";
 
-function TodoSearch(){
+function TodoSearch({searchValue, setSearchValue}){
 
-    const {searchValue, setSearchValue}= React.useContext(TodoContext)
 
-   
     const onSearchValueChange =(event)=>{
         console.log(event.target.value)
         setSearchValue(event.target.value)
